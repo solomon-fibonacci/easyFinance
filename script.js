7b1e0e20f42a8a8da7949cefd2fa6e1fea8ac46c
Solomon Omojola <omojolasolomon@gmail.com>
2016-10-27 08:28:04 +0100

    Create initial JS code structure

--- script.js
+++ script.js
@@ -1,3 +1,122 @@
 var financeApp = {
-	
-}
+	init: function() {
+    this.loadData();
+    this.cacheDom();
+    this.bindEvents();
+    this.render();
+  },
+
+  loadData: function() {
+    if ($.trim(localStorage.getItem('transactions')).length > 0) {
+      this.tasks = $.parseJSON(localStorage.getItem('transactions')).tasks;
+    } else {
+      this.tasks = [];
+    }
+    this.displayDate = moment().format("ddd, Do MMM");
+  },
+
+  cacheDom: function() {
+    this.$doc = $('#container');
+    this.$forwardButton = this.$doc.find('#forward');
+    this.$backButton = this.$doc.find('#back');
+    this.$displayDate = this.$doc.find('h2');
+    this.$inputForm = this.$doc.find('#inputForm');
+    this.$input = this.$doc.find('#inputText');
+    this.$errorSpan = this.$doc.find('#errorSpan');
+    this.$addButton = this.$doc.find('#addItem');
+    this.$listDiv = this.$doc.find('#listDiv');
+    this.$ul = this.$doc.find('ul');
+    this.template = this.$doc.find('#todoTemplate').html();
+  },
+
+  bindEvents: function() {
+    this.$forwardButton.on('click', this.goForward.bind(this));
+    this.$backButton.on('click', this.goBack.bind(this));
+    this.$inputForm.on('submit', this.addItem.bind(this));
+    this.$input.on('focus', this.render.bind(this));
+    this.$listDiv.on('change', '.todoCheckbox', this.tickItem.bind(this));
+    this.$listDiv.on('click', '.delButton', this.deleteItem.bind(this));
+    this.$listDiv.on('click', '.editButton', this.renderEditBox.bind(this));
+    this.$listDiv.on('submit', '.editBox', this.updateItem.bind(this));
+  },
+
+  saveTransactions: function() {
+    var ingoingData = JSON.stringify({ tasks: this.tasks });
+    localStorage.setItem('todos', ingoingData);
+  },
+
+  filterTransactions: function() {
+    var today = moment();
+    var filterDate = moment(this.displayDate, 'ddd, Do MMM');
+    var filteredTasks = this.tasks.filter(function(t) {
+      var taskDate = moment(t.date, 'DDMMYYYY');
+      return (
+        taskDate.isSame(filterDate, 'day') && t.ischecked ||
+        filterDate.isSame(today, 'day') && !t.ischecked && today.isSameOrAfter(taskDate, 'day') ||
+        taskDate.isSame(filterDate, 'day')  && filterDate.isAfter(today, 'day')
+      );
+    });
+    var data = {
+      tasks: filteredTasks,
+    };
+    return data;
+  },
+
+  render: function() {
+    var data = this.filterTasks(),
+      todaysDate = moment();
+    this.$input.val('');
+    this.$ul.html(Mustache.render(this.template, data));
+    this.$displayDate.html(this.displayDate);
+    if (moment(this.displayDate, 'ddd, Do MMM').isBefore(todaysDate, 'day')) {
+      this.$input.prop('disabled', true);
+      this.$ul.children('li').find('.editButton').hide();
+    } else {
+      this.$input.prop('disabled', false);
+    }
+    this.renderCheckboxes();
+  },
+
+
+  renderError: function(errorType) {
+    console.log('Hey there!');
+    var errorMsg = errorType == 'longer' ? 'Please limit your entry to 140 characters.' : 'Entry cannot be empty.';
+    console.log(errorMsg);
+    this.$errorSpan.html(errorMsg);
+    console.log(this.$errorSpan);
+    this.$errorSpan.fadeIn(1000);
+    this.$errorSpan.delay(10000).fadeOut(1000);
+  },
+
+  renderEditBox: function(event) {
+    var $items = $(event.target).closest('ul').find('.todoItemText');
+    var $boxes = $(event.target).closest('ul').find('.editBox');
+    $boxes.hide();
+    $items.fadeIn(150);
+    var $text = $(event.target).closest('li').find('.todoItemText');
+    $text.fadeOut(50, function() {
+      var $box = $(event.target).closest('li').find('.editBox');
+      $box.children('input').val($.trim($text.contents().text()));
+      $box.addClass('visibleEditBox').fadeIn(150);
+    });
+  },
+  
+  addTransaction: function(event) {
+  },
+
+  updateTransaction: function(event) {
+  },
+
+
+  deleteTransaction: function() {
+  },
+
+  goBack: function() {
+  },
+
+  goForward: function() {
+  },
+
+};
+
+financeApp.init();
